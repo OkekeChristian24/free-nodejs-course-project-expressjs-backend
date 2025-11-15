0128b84bdd836c4647ae461795fb3e3cbd61305b
Okeke Christian Chimezie <christian.okeke24@gmail.com>
2025-11-15 12:51:39 +0100

    chore: new updates

--- mysql_tutorial.sql
+++ mysql_tutorial.sql
@@ -18,3 +18,94 @@ CREATE TABLE students (
 
 -- To describe a table
 DESCRIBE students;
+
+-- To delete a table
+DROP TABLE students;
+
+/*
+Key ones to learn:
+INT – whole numbers
+DECIMAL(10,2) – money values
+VARCHAR(n) – text with limit
+TEXT – long text
+DATE, DATETIME
+BOOLEAN
+JSON (MySQL 5.7+)
+
+Example:
+price DECIMAL(10,2)
+created_at DATETIME
+*/
+
+-- Insert, Select, Update, Delete (CRUD)
+INSERT INTO students (name, age)
+VALUES ('John Doe', 18);
+
+SELECT name, age FROM students;
+
+UPDATE students
+SET age = 19
+WHERE id = 1;
+
+DELETE FROM students
+WHERE id = 1;
+
+
+-- Filtering Data (WHERE clause)
+SELECT * FROM students
+WHERE age > 18;
+
+/*
+Operators:
+=, !=, >, <, >=
+IN, BETWEEN
+LIKE (pattern matching)
+*/
+
+SELECT * FROM students
+WHERE name LIKE 'J%';
+
+-- Sorting & Limiting
+SELECT * FROM students
+ORDER BY age DESC;
+
+SELECT * FROM students
+LIMIT 5;
+
+-- Relationships & Keys
+/*
+Types:
+Primary Key (unique identifier)
+Foreign Key (references another table)
+One-to-Many, Many-to-Many
+*/
+CREATE TABLE courses (
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   title VARCHAR(50)
+);
+
+CREATE TABLE enrollments (
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   student_id INT,
+   course_id INT,
+   FOREIGN KEY (student_id) REFERENCES students(id),
+   FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+-- Joins
+-- Inner Join
+SELECT students.name, courses.title
+FROM enrollments
+INNER JOIN students ON enrollments.student_id = students.id
+INNER JOIN courses ON enrollments.course_id = courses.id;
+
+
+-- Left Join
+SELECT s.name, e.course_id
+FROM students s
+LEFT JOIN enrollments e ON s.id = e.student_id;
+
+-- Indexes
+-- Improve search performance.
+CREATE INDEX idx_student_name
+ON students(name);
---
+++ project_sql_schema.sql
@@ -0,0 +1,68 @@
+-- Create user and grant privileges 
+CREATE USER 'blog_user'@'localhost' IDENTIFIED BY 'password1';
+GRANT ALL PRIVILEGES ON my_blog.* TO 'blog_user'@'localhost';
+FLUSH PRIVILEGES;
+SELECT User, Host FROM mysql.user;
+
+
+CREATE DATABASE my_blog;
+
+USE my_blog;
+
+-- Create 'user' table
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    bio TEXT,
+	password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+-- Create 'post' table
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+	user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES user(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+CREATE INDEX idx_post_created_at ON post(created_at);
+
+-- So you can efficiently fetch post (all) for a user and order by created_at
+/*
+SELECT *
+FROM post
+WHERE user_id = 123
+ORDER BY created_at DESC
+LIMIT 10 OFFSET 0;
+*/
+CREATE INDEX idx_post_user_id_created_at ON post(user_id, created_at);
+
+-- Create 'comment' table with foreign key relation to 'post'
+CREATE TABLE comment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+	user_id INT NOT NULL,
+    text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES post(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES user(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+
+-- So you can efficiently fetch comment (all) for a post and order by created_at
+CREATE INDEX idx_comment_post_id_created_at ON comment(post_id, created_at);
+
